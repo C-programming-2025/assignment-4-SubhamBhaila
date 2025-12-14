e57c5071539f165c6f684e958f26bd7def39da2b
SubhamBhaila <subhambhaila10@gmail.com>
2025-12-14 22:09:05 +0545

    Update q2.c

--- src/q2.c
+++ src/q2.c
@@ -1,1 +1,26 @@
 // Write a C program that reverses an array of characters using pointers.
+#include <stdio.h>
+
+int main() {
+    char arr[100];
+    char *start, *end, temp;
+
+    printf("Enter a string: ");
+    gets(arr);
+
+    start = arr;
+    end = arr;
+    while (*end) end++;
+    end--;
+
+    while (start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+
+    printf("Reversed string: %s\n", arr);
+    return 0;
+}
