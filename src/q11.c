27dad361a3d684d07a799009966a9936fcce978e
SubhamBhaila <subhambhaila10@gmail.com>
2025-12-14 22:07:18 +0545

    Update q11.c

--- src/q11.c
+++ src/q11.c
@@ -1,2 +1,18 @@
 // Write a C program to create a file and write a user-entered string into it. Then display a 
 // message confirming that the file has been written successfully.
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char str[100];
+
+    fp = fopen("output.txt", "w");
+    printf("Enter a string: ");
+    gets(str);
+
+    fputs(str, fp);
+    fclose(fp);
+
+    printf("File written successfully.\n");
+    return 0;
+}
