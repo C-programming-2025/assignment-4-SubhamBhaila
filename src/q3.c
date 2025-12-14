5fb08c12145047b1836097fe4c4cc21d864d0427
SubhamBhaila <subhambhaila10@gmail.com>
2025-12-14 22:09:22 +0545

    Update q3.c

--- src/q3.c
+++ src/q3.c
@@ -1,1 +1,26 @@
 // Write a C program that checks if a given string is a palindrome using pointers.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    char *start, *end;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    start = str;
+    end = str + strlen(str) - 1;
+
+    while (start < end) {
+        if (*start != *end) {
+            printf("Not a palindrome\n");
+            return 0;
+        }
+        start++;
+        end--;
+    }
+
+    printf("Palindrome\n");
+    return 0;
+}
