7d173bdaf7765edd53951c9e5079ae4dd854165f
SubhamBhaila <subhambhaila10@gmail.com>
2025-12-14 22:08:23 +0545

    Update q14.c

--- src/q14.c
+++ src/q14.c
@@ -1,1 +1,22 @@
 // Write a C program that counts the number of characters, words, and lines in a text file.
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int chars = 0, words = 0, lines = 0;
+
+    fp = fopen("text.txt", "r");
+
+    while ((ch = fgetc(fp)) != EOF) {
+        chars++;
+        if (ch == ' ' || ch == '\n')
+            words++;
+        if (ch == '\n')
+            lines++;
+    }
+
+    fclose(fp);
+    printf("Characters: %d\nWords: %d\nLines: %d\n", chars, words + 1, lines);
+    return 0;
+}
