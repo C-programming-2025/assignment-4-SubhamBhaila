1c89bb8c19e3d062002ecd05d452cbb2a4fc5775
SubhamBhaila <subhambhaila10@gmail.com>
2025-12-14 22:10:25 +0545

    Update q6.c

--- src/q6.c
+++ src/q6.c
@@ -1,2 +1,19 @@
 // Write a C program that defines a structure called Student with members: name, roll number, 
 // and marks. Prompt the user to enter data for one student and display the information.
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    printf("Enter name, roll, marks:\n");
+    scanf("%s %d %f", s.name, &s.roll, &s.marks);
+
+    printf("Name: %s\nRoll: %d\nMarks: %.2f\n", s.name, s.roll, s.marks);
+    return 0;
+}
